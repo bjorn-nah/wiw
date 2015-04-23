b8532d8cf7bbab1312512cc6bccb0e7a0c65a4e4
bjorn-nah <bjorn.nah@gmail.com>
2015-04-23 22:27:40 +0200

    ajout second joueur

--- alice_event.c
+++ alice_event.c
@@ -22,23 +22,23 @@
 
 int tics_alice, frame_alice, state_alice, tics_lapin, frame_lapin, state_lapin, counter;
 int j1, j2, alice_x, alice_y, alice_v, lapin_x, lapin_y, lapin_v, ball_x, ball_y, ball_vect_x, ball_vect_y;
-int point_alice, point_lapin, play_state;
+int point_alice, point_lapin, play_state, max;
 char dir_alice, dir_lapin, walk_alice, walk_lapin;
 
 main()
 {
 	
-	spr_make(0,180,121,0x5000,FLIP_MAS|SIZE_MAS,NO_FLIP|SZ_32x64,0,1);
+	spr_make(0,180,121,0x5000,FLIP_X_MASK|SIZE_MAS,FLIP_X|SZ_32x64,0,1);
 	set_sprpal(0,lapinpal,1);
-	load_vram(0x5000,lapin,0x600);
+	load_vram(0x5000,lapin,0xA00);
 
 	spr_make(1,52,121,0x6200,FLIP_MAS|SIZE_MAS,NO_FLIP|SZ_32x64,1,1);
 	set_sprpal(1,alicepal,1);
-	load_vram(0x6000,alice,0x1000);	
+	load_vram(0x6000,alice,0xA00);	
 
-	spr_make(2,200,100,0x5900,FLIP_MAS|SIZE_MAS,NO_FLIP|SZ_16x16,2,1);
+	spr_make(2,200,100,0x5A00,FLIP_MAS|SIZE_MAS,NO_FLIP|SZ_16x16,2,1);
 	set_sprpal(2,ballpal,1);
-	load_vram(0x5900,ball,0x100);
+	load_vram(0x5A00,ball,0x100);
 
 	load_background(scene_chr,scene_pal,scene_bat,32,28);
 
@@ -56,6 +56,8 @@ main()
 	lapin_y = 121;
 	alice_v = 0;
 	lapin_v = 0;
+	dir_alice = DIR_RIGHT;
+	dir_lapin = DIR_LEFT;
 
 	ball_x = 200;
 	ball_y = 100;
@@ -65,6 +67,8 @@ main()
 	point_alice = 0;
 	point_lapin = 0;
 	play_state = 0;
+	max = 15;
+
 
 	set_font_color(13, 0);
 	load_default_font();
@@ -86,9 +90,9 @@ main()
 		/*
 		put_number(counter, 5 , 5, 0);
 		put_number(point_alice, 5 , 5, 1);
-		put_number(point_lapin, 5 , 5, 2); */
+		put_number(point_lapin, 5 , 5, 2);
 		put_number(ball_vect_x, 5 , 5, 3);
-		/* put_number(ball_vect_y, 5 , 5, 4);
+		put_number(ball_vect_y, 5 , 5, 4);
 		put_number(play_state, 5 , 5, 5);
 		*/
 		counter++;
@@ -117,18 +121,19 @@ player_machine()
 	if ( state_alice == 5) player_alice_5();
 	if ( state_alice == 6) player_alice_6();
 
-	/*
+	
 	spr_set(0);
-	if ( state_lapin == 0) then player_lapin_0();
-	if ( state_lapin == 1) then player_lapin_1();
-	if ( state_lapin == 2) then player_lapin_2();
-	if ( state_lapin == 3) then player_lapin_3();
-	if ( state_lapin == 4) then player_lapin_4();
-	if ( state_lapin == 5) then player_lapin_5();
-	if ( state_lapin == 6) then player_lapin_6();
-	*/
+	if ( state_lapin == 0) player_lapin_0();
+	if ( state_lapin == 1) player_lapin_1();
+	if ( state_lapin == 2) player_lapin_2();
+	if ( state_lapin == 3) player_lapin_3();
+	if ( state_lapin == 4) player_lapin_4();
+	if ( state_lapin == 5) player_lapin_5();
+	if ( state_lapin == 6) player_lapin_6();
+	
 }
 
+/* joueur ALice */
 player_alice_0()
 {
 	if ( tics_alice == 0 ) spr_pattern(0x6200);
@@ -265,15 +270,159 @@ player_alice_6()
 	}
 }
 
+/* joueur Lapin */
+player_lapin_0()
+{
+	if ( tics_lapin == 0 ) spr_pattern(0x5200);
+	tics_lapin = 1;
+	if (j2 & JOY_LEFT)
+	{
+		/* spr_ctrl(FLIP_X_MASK,FLIP_X); */
+		tics_lapin = 0;
+		frame_lapin = 0;
+		state_lapin = 1;
+		spr_pattern(0x5200);
+		dir_lapin = DIR_LEFT;
+	}
+	if (j2 & JOY_RGHT)
+	{
+		/* spr_ctrl(FLIP_X_MASK,NO_FLIP_X); */
+		tics_lapin = 0;
+		frame_lapin = 0;
+		state_lapin = 1;
+		spr_pattern(0x5000);
+		dir_lapin = DIR_RIGHT;
+	}
+	if (j2 & JOY_A)
+	{
+		tics_lapin = 0;
+		frame_lapin = 0;
+		state_lapin = 3;
+		spr_pattern(0x5000);
+	}
+}
+
+player_lapin_1()
+{
+	walk_lapin = 0;
+	tics_lapin++;
+	if (tics_lapin > 6)
+	{
+		tics_lapin = 0;
+		frame_lapin++;
+		if (frame_lapin > 3) frame_lapin = 0;
+		if (frame_lapin == 0) spr_pattern(0x5200);
+		if (frame_lapin == 1) spr_pattern(0x5000);
+		if (frame_lapin == 2) spr_pattern(0x5200);
+		if (frame_lapin == 3) spr_pattern(0x5400);
+	}
+	if (j2 & JOY_LEFT && dir_lapin == DIR_LEFT)
+	{
+		walk_lapin = 1;
+	}
+	if (j2 & JOY_RGHT && dir_lapin == DIR_RIGHT)
+	{
+		walk_lapin = 1;
+	}
+	if (j2 & JOY_A)
+	{
+		tics_lapin = 0;
+		frame_lapin = 0;
+		state_lapin = 3;
+		spr_pattern(0x5000);
+	}
+	if (walk_lapin == 0)
+	{
+		tics_lapin = 0;
+		frame_lapin = 0;
+		state_lapin = 0;
+	}
+	
+	else
+	{
+		if(dir_lapin == DIR_LEFT)
+		{
+			lapin_x-=2;
+		}
+		else
+		{
+			lapin_x+=2;
+		}
+	
+	}
+		
+}
+
+player_lapin_2()
+{
+return;
+}
+
+player_lapin_3()
+{
+	if (lapin_y == 121) lapin_v = 10;
+	if (j2 & JOY_LEFT)
+	{
+		dir_lapin = DIR_LEFT;
+		lapin_x-=2;
+	}
+	if (j2 & JOY_RGHT)
+	{
+		dir_lapin = DIR_RIGHT;
+		lapin_x+=2;
+	}
+	if (lapin_v == 0) state_lapin = 4;
+}
+
+player_lapin_4()
+{
+	if (j2 & JOY_LEFT)
+	{
+		dir_lapin = DIR_LEFT;
+		lapin_x-=2;
+	}
+	if (j2 & JOY_RGHT)
+	{
+		dir_lapin = DIR_RIGHT;
+		lapin_x+=2;
+	}
+	if (lapin_y == 121)  state_lapin = 0;
+}
+
+player_lapin_5()
+{
+return;
+}
+
+player_lapin_6()
+{
+	play_state = 1;
+	if (tics_lapin == 0) spr_pattern(0x5800);
+	if (tics_lapin == 4) spr_pattern(0x5600);
+	tics_lapin++;
+	if (tics_lapin > 8)
+	{
+		tics_lapin = 0;
+		state_lapin = 0;
+	}
+}
+
 physique()
 {
-
+	/* a coller en fonction? */
 	if (alice_x < -8) alice_x = -8;
 	if (alice_x > 96) alice_x = 96;
 	alice_y = alice_y - alice_v;
 	if (alice_y < 121) alice_v--;
 	if (alice_y > 121) alice_y = 121;
 	spr_put(1,alice_x,alice_y, dir_alice);
+
+	if (lapin_x < 128) lapin_x = 128;
+	if (lapin_x > 232) lapin_x = 232;
+	lapin_y = lapin_y - lapin_v;
+	if (lapin_y < 121) lapin_v--;
+	if (lapin_y > 121) lapin_y = 121;
+	spr_put(0,lapin_x,lapin_y, dir_lapin);
 
 	/* collisions balle fillet */
 	rebond(4, 112, 121, 15);
@@ -306,13 +455,13 @@ int truc_x, truc_y, sprite_num, vect;
 	{
 		ball_vect_y = vect * (SLOW/2);
 		ball_vect_x = diff * (SLOW/2);
-		/*
+		
 		if (sprite_num == 0) 
 		{
 			state_lapin = 6;
 			tics_lapin = 0;
 		}
-		*/
+		
 		if (sprite_num == 1) 
 		{
 			state_alice = 6;
@@ -334,8 +483,7 @@ score()
 			play_state = 1;
 		}
 	}
-	else
-
+	
 	if (ball_y > 150) 
 	{
 		ball_vect_x = 0;
@@ -354,6 +502,15 @@ score()
 			point_alice++;
 			ball_x = 42;
 		}
+		put_number(point_alice, 2 , 6, 0);
+		put_number(point_lapin, 2 , 24 , 0);
+	}
+	
+	if(point_alice == max | point_lapin == max)
+	{
+		point_alice = 0;
+		point_lapin = 0;
+		play_state = 0;
 		put_number(point_alice, 2 , 6, 0);
 		put_number(point_lapin, 2 , 24 , 0);
 	}
